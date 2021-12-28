ece63c5253d1052b69ef52a2b8aec3b08bd66228
grigolenok <egor.grigolenok@mail.ru>
2021-12-28 10:40:09 -0800

    first commit

---
+++ src/test/kotlin/com/example/mBankAuthorization/MBankAuthorizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mBankAuthorization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MBankAuthorizationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
